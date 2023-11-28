5533b16973661073a45ad3e88b322694cea0668a
Disguised-Coffee <68920714+Disguised-Coffee@users.noreply.github.com>
2023-11-28 23:57:40 +0000

    Finished Assignment

--- jsLab.js
+++ jsLab.js
@@ -16,27 +16,33 @@ let tile9 = document.querySelector('.tile-9')
 // 1. Tile 1 feels a little sad. 
 // - Update the inner HTML to show an encouraging message! 
 // - Something like "YOU GOT THIS!" or even a "You're a beautiful person!" 
-
+  tile1.innerHTML = "YOU GOT THIS!";
 
 
 // 2. Tile 2 has a button. 
 // - Create a click event and have the background color of tile 2 change to a shade of green.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
 // - Can you see if there's a way to toggle it???
+let button = document.querySelector('button');
 
+button.addEventListener("click", function() {
+  tile2.style.backgroundColor = "green";
+  tile2.appendChild(button);
+});
 
 // 3. What's this? A totoro? Where's his friends? 
 // - Using createElement, create a friend (an image element).
 // - Once created, set the src of it to the image address.
 // - Append the element to tile 3. 
 // - Need some inspiration? Go to giphy.com, search a character and choose the "stickers".
+let friend = document.createElement("img");
+friend.src = "https://i.imgur.com/anBZrEb.jpeg";
 
+tile3.appendChild(friend);
 
 // 4. Oh no! A broken image! We need to fix this.
 // - Set the src for tile 4 to have an image of your favorite TV show.
-
-
-
+tile4.src = "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/aaee4e34242e3abc0af317edbada66aa.jpe";
 
 // These following tasks may require a little bit of digging. 
 // Unsure how to complete the task? Use your resources! Code Nation reference table might help. 
@@ -46,20 +52,33 @@ let tile9 = document.querySelector('.tile-9')
 
 // 5. Hmm. Looks like the font is too small. 
 // - Update the font-size of tile 5 to 35 pixels.
-
+tile5.style.fontSize = "35px";
 
 // 6. Another sad box! 
 // - Let make this tile 6 feel special and add a gradient background! 
 // - What kind of gradient? What color? YOU CHOOSE!
+tile6.style.background = "linear-gradient(to bottom left, cyan 25%, palegoldenrod 50%)"
 
 // 7. Too many squares. 
 // - Let's change tile 7 to have rounded edges - be sure to search for which CSS property does this :)
+tile7.style.borderRadius = "25px";
 
 // 8. Replace the image with your favorite emoji. 
 // - Try going to this site to search: https://emojipedia.org/
+tile8.src = "https://emojigraph.org/media/emojidex/alien_1f47d.png";
 
 // 9. Display a random number in the tile when the button is clicked.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
+
+let random = document.querySelector(".random-button");
+random.addEventListener("click", function() {
+let nub = (Math.round(Math.random() * 9000 + 2)/(10 * (Math.round(Math.random() * 8))) );
+  let apara = document.createElement("p");
+  apara.innerHTML = nub;
+  apara.style.fontSize = "200px";
+  apara.style.fontFamily = "Helvetta"
+  tile9.appendChild(apara);
+});
 
 
 
